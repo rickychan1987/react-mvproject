5301bd1bdeb82a3665ddadc7077027b8e16f903c
rickychan1987 <khaivanla1987@gmail.com>
2021-10-23 20:58:14 +0700

    Starting to code with components

--- react-mvapp/src/GlobalStyle.js
+++ react-mvapp/src/GlobalStyle.js
@@ -16,19 +16,28 @@ export const GlobalStyle = createGlobalStyle `
   * {
     box-sizing: border-box;
     font-family: 'Abel', sans-serif;
+  }
 
-    body {
-      margin: 0;
-      padding: 0;
+  body {
+    margin: 0;
+    padding: 0;
 
     h1 {
       font-size: 2rem;
       font-weight: 600;
-      color:
+      color: var(--white);
     }
     
+    h3 {
+      font-size: 1.1rem;
+      font-weight: 600;
+    }
+    
+    p {
+      font-size: 1rem;
+      color: var(--white);
     }
   }
 
 
-`
+`;
