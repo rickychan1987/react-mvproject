6e8a18f0919d0830b046e8ddd8ab6f96d9cd48cc
rickychan1987 <khaivanla1987@gmail.com>
2021-10-25 23:59:29 +0700

    3h Done searchbar

--- react-mvapp/src/App.js
+++ react-mvapp/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// components
+import Header from './components/Header';
+import Home from './components/Home';
 
 //styles
 import { GlobalStyle } from './GlobalStyle';
@@ -6,7 +9,8 @@ import { GlobalStyle } from './GlobalStyle';
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header/>
+      <Home/>
       <GlobalStyle />
     </div>
   );
