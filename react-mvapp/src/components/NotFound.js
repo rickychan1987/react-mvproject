dde0e37d1dd0922b3a493713e035d059370f2067
rickychan1987 <khaivanla1987@gmail.com>
2021-10-26 22:01:23 +0700

    Movie Page 3h34

---
+++ react-mvapp/src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <div>NotFoud</div>;
+
+export default NotFound;
--- react-mvapp/src/hooks/useHomeFetch.js
+++ react-mvapp/src/hooks/useHomeFetch.js
@@ -14,7 +14,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = '') => {
         try {
@@ -36,8 +36,17 @@ export const useHomeFetch = () => {
 
     // Initial render
     useEffect(() => {
-        fetchMovies(1);
-    }, []);
+        setState(initialState)
+        fetchMovies(1, searchTerm);
+    }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm };
+    // Load More
+    useEffect(() => {
+        if (!isLoadingMore) return;
+
+        fetchMovies(state.page + 1, searchTerm);
+        setIsLoadingMore(false);
+    }, [isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore };
 };
